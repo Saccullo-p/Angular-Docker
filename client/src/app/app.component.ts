e18fef832d5e75155399d779a6ffea8f95390b22
Saccullo-p <saccullo.pietro@iisgalvanimi.edu.it>
2022-12-20 14:24:43 +0000

    04

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,22 +1,46 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
+import { AnimalsService } from './animals.service';
+import { Animal, VettAnimal } from './animals.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Test';
-  data = {};
-  constructor(private http: HttpClient)
+  title = 'client';
+  data = new Array<Animal>();
+  //Mi faccio iniettare l'animal servce
+  constructor(private animalService : AnimalsService)
   {
-    this.http.get("https://5000-saccullop-angulardocker-f213o1tkshc.ws-eu79.gitpod.io/simple_json")
-    .subscribe(
-      (data) => 
-      {
-        this.data = data;
+    //Mi sottoscrivo al servizio
+    this.animalService.getAnimals().subscribe(
+      (data: VettAnimal)=>{this.data = data['animals']}
+    )
+  }
+
+  //Per la reactive form creo due proprietà che conterranno i valori delle caselle di testo
+  form = new FormGroup({
+    "name": new FormControl(),
+    "type": new FormControl(),
+  });
+
+  onSubmit() {
+    console.log("reactive form submitted");
+    console.log(this.form.controls['name'].value);
+    console.log(this.form.controls['type'].value);
+    let a : Animal = {
+      "id": '0',
+      "name":this.form.controls['name'].value,
+      "type": this.form.controls['type'].value
+    };
+
+    //Quando ricevo una risposta dal server aggiorno l'id dell'animale e lo invio al vettore data
+    this.animalService.sendNewAnimal(a).subscribe(
+      (data)=>{
         console.log(data);
+        a.id = data['id'];
+        this.data.push(a);
       }
     )
   }
---
+++ client/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    baseUrlServer: 'https://4200-saccullop-angulardocker-melhyhwm0os.ws-eu79.gitpod.io/'
+  };
